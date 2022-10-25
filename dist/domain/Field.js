ed536952da613c9d1d7aee070c7a9173db79ce78
Shadrina Alyona <alyon.shadrina@gmail.com>
2022-10-25 15:40:19 +0300

    [fix]: consider win when win coordinated are [0,0]

--- dist/domain/Field.js
+++ dist/domain/Field.js
@@ -49,6 +49,18 @@ class Field {
         // get surronding cells with relevant symbol
         const existingSurroundingCoordinates = potentialySurroundingCoordinates.filter(coordinatesStrings => this.cells[coordinatesStrings] === symbol);
         // check if next cell within provided vector has relevant symbol
+        const checkPrevCellSymbol = (coordinate, vector, potentionalyWinRow) => {
+            console.log('checkPrevCellSymbol', coordinate, vector, potentionalyWinRow);
+            const prevPotentialCoordinate = [coordinate[0] - vector[0], coordinate[1] - vector[1]];
+            const prevPotentialCoordinateString = JSON.stringify(prevPotentialCoordinate);
+            if (this.cells[prevPotentialCoordinateString] === symbol) {
+                potentionalyWinRow.push(prevPotentialCoordinate);
+                if (potentionalyWinRow.length !== n) {
+                    checkPrevCellSymbol(prevPotentialCoordinate, vector, potentionalyWinRow);
+                }
+            }
+        };
+        // check if next cell within provided vector has relevant symbol
         const checkNextCellSymbol = (coordinate, vector, potentionalyWinRow) => {
             const nextPotentialCoordinate = [coordinate[0] + vector[0], coordinate[1] + vector[1]];
             const nextPotentialCoordinateString = JSON.stringify(nextPotentialCoordinate);
@@ -59,19 +71,38 @@ class Field {
                 }
             }
         };
+        console.log('coordinates', coordinates);
+        console.log('existingSurroundingCoordinates', existingSurroundingCoordinates);
         for (let i = 0; i < existingSurroundingCoordinates.length; i++) {
             const currentCoordinatesString = existingSurroundingCoordinates[i];
             const currentCoordinatesNumber = JSON.parse(currentCoordinatesString);
             const currentCoordinatesX = currentCoordinatesNumber[0];
             const currentCoordinatesY = currentCoordinatesNumber[1];
             const potentionalyWinRow = [coordinates, currentCoordinatesNumber];
+            console.log('coordinates', coordinates);
             const vector = [currentCoordinatesX - coordinates[0], currentCoordinatesY - coordinates[1]];
+            console.log('vector', vector);
             const nextPotentialCoordinate = [currentCoordinatesX + vector[0], currentCoordinatesY + vector[1]];
+            console.log('nextPotentialCoordinate', nextPotentialCoordinate);
             const nextPotentialCoordinateString = JSON.stringify(nextPotentialCoordinate);
             if (this.cells[nextPotentialCoordinateString] === symbol) {
                 potentionalyWinRow.push(nextPotentialCoordinate);
                 if (potentionalyWinRow.length !== n) {
                     checkNextCellSymbol(nextPotentialCoordinate, vector, potentionalyWinRow);
+                }
+            }
+            // if no more next, check prev
+            if (potentionalyWinRow.length !== n) {
+                console.log('----if no more next, check prev');
+                const prevPotentialCoordinate = [coordinates[0] - vector[0], coordinates[1] - vector[1]];
+                console.log('prevPotentialCoordinate', prevPotentialCoordinate);
+                const prevPotentialCoordinateString = JSON.stringify(prevPotentialCoordinate);
+                if (this.cells[prevPotentialCoordinateString] === symbol) {
+                    console.log('this.cells[prevPotentialCoordinateString]', this.cells[prevPotentialCoordinateString]);
+                    potentionalyWinRow.push(prevPotentialCoordinate);
+                    if (potentionalyWinRow.length !== n) {
+                        checkPrevCellSymbol(prevPotentialCoordinate, vector, potentionalyWinRow);
+                    }
                 }
             }
             if (potentionalyWinRow.length === n) {
--- src/domain/Field.ts
+++ src/domain/Field.ts
@@ -42,7 +42,7 @@ export class Field implements IField {
     return new ActionResultError('Coordinates are not available', null);
   }
 
-  // TODO: has bug: doesn't consider win when win `coordinates` are [0, 0]
+  // TODO: create registry of already checked surrounding coordinates
   hasNSymbolsInRow(coordinates: [number, number], symbol: string, n: number): boolean {
     // get all surronding cells
     const potentialySurroundingCoordinates = [
@@ -62,6 +62,16 @@ export class Field implements IField {
     );
 
     // check if next cell within provided vector has relevant symbol
+    const checkPrevCellSymbol = (coordinate: TCoordinates, vector: TCoordinates, potentionalyWinRow: any) => {
+      const prevPotentialCoordinate: TCoordinates = [coordinate[0] - vector[0], coordinate[1] - vector[1]];
+      const prevPotentialCoordinateString = JSON.stringify(prevPotentialCoordinate);
+      if (this.cells[prevPotentialCoordinateString] === symbol) {
+        potentionalyWinRow.push(prevPotentialCoordinate)
+        if (potentionalyWinRow.length !== n) {
+          checkPrevCellSymbol(prevPotentialCoordinate, vector, potentionalyWinRow)
+        }
+      }
+    }
     const checkNextCellSymbol = (coordinate: TCoordinates, vector: TCoordinates, potentionalyWinRow: any) => {
       const nextPotentialCoordinate: TCoordinates = [coordinate[0] + vector[0], coordinate[1] + vector[1]];
       const nextPotentialCoordinateString = JSON.stringify(nextPotentialCoordinate);
@@ -82,12 +92,25 @@ export class Field implements IField {
       const potentionalyWinRow = [coordinates, currentCoordinatesNumber];
 
       const vector: TCoordinates = [currentCoordinatesX - coordinates[0], currentCoordinatesY - coordinates[1]];
-      const nextPotentialCoordinate: TCoordinates = [currentCoordinatesX + vector[0], currentCoordinatesY + vector[1]];
+
+      // check next coordinates in provided vector
+      const nextPotentialCoordinate: TCoordinates = [currentCoordinatesX + vector[0], currentCoordinatesY + vector[1]];      
       const nextPotentialCoordinateString = JSON.stringify(nextPotentialCoordinate);
       if (this.cells[nextPotentialCoordinateString] === symbol) {
         potentionalyWinRow.push(nextPotentialCoordinate)
         if (potentionalyWinRow.length !== n) {
           checkNextCellSymbol(nextPotentialCoordinate, vector, potentionalyWinRow)
+        }
+      }
+      // if no more next, check prev coordinates in provided -vector
+      if (potentionalyWinRow.length !== n) {
+        const prevPotentialCoordinate: TCoordinates = [coordinates[0] - vector[0], coordinates[1] - vector[1]];
+        const prevPotentialCoordinateString = JSON.stringify(prevPotentialCoordinate);
+        if (this.cells[prevPotentialCoordinateString] === symbol) {
+          potentionalyWinRow.push(prevPotentialCoordinate)
+          if (potentionalyWinRow.length !== n) {
+            checkPrevCellSymbol(prevPotentialCoordinate, vector, potentionalyWinRow)
+          }
         }
       }
       if (potentionalyWinRow.length === n) {
